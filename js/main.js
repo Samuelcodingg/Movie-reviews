8a635690567e751b78ca772c6f32179922346c2c
Samuelcodingg <samuelroman39@gmail.com>
2021-08-18 16:18:56 -0500

    animations with scrollreveals library ok

--- js/main.js
+++ js/main.js
@@ -2,6 +2,31 @@
 //when the DOM content is loaded
 $(document).ready(function(){
 
+    //animations with scrollreveals library
+    window.sr = ScrollReveal();
+    sr.reveal('#phrase',{
+        duration: 3000
+    });
+
+    sr.reveal('.fa-film',{
+        duration: 3000
+    });
+
+    sr.reveal('#about-this', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('#known-movies', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-250px'
+    });
+
+    sr.reveal('#searcher', {
+        duration: 3000
+    });
 
     //scroll
     $('#moveToSearcher').click(function(e){
